4dab0d2c353b6fe213cd80b6129169716d7ca3b4
Andres <afsanchezsa@unal.edu.co>
2020-01-20 15:47:36 -0800

    solucionado problema en funcion Colisiona

--- entrega2/funciones.c
+++ entrega2/funciones.c
@@ -17,10 +17,11 @@
 #define BACKLOG 32
 #define NUMHILOS 32
 #define tam 100151
-char minuscula[33];
+
 
 
 char * toLower(char *arr){
+  char *minuscula=(char*)malloc(sizeof(char)*33);
     int i;
    // char * minuscula=(char *)malloc(sizeof(char)*33); 
 for(i=0;i<33;i++){
@@ -262,19 +263,28 @@ int indiceTaildelaLista(int indice_front){
 //compara si existe y si los nombres son iguales, colisiona si hay mismo hash pero diferente nombre
 int  Colisiona(char *nombre_nuevo,int posicion){//retorna -1 si no hay colision y 1 si la hay
   if(posicion==-1){
+    printf("entro al especial");
     return -1;
   }
   struct Nodo *leido=LeerdeBD(posicion);
-  if(strcmp(toLower(leido->mascota.nombre),toLower(nombre_nuevo))==0){  
-    
+  char * cadena1=toLower(leido->mascota.nombre);
+  char * cadena2=toLower(nombre_nuevo);
+  printf("resultado del comp es %i",strcmp(toLower(leido->mascota.nombre),toLower(nombre_nuevo)));
+  int res=strcmp(cadena1,cadena2);
+  free(cadena1);
+  free(cadena2);
+  if(res==0){
+      
+    printf("%s no colisiona con %s  (-1)\n",toLower(nombre_nuevo),toLower(leido->mascota.nombre));
     return -1;
   }
-  
+   printf("%s colisiona con %s (1)\n",nombre_nuevo,leido->mascota.nombre);
   return 1;
 }
 
 //retorna el cuadrado, si hay colisión suma valor al hash
 int sondeo(int i){
+if(i>0)printf("sondea");
   return (i*i);
 }
 
@@ -406,12 +416,17 @@ void DesconectarMascota(int posicion){
     free(siguiente);
   }else if(eliminado->anterior!=-1){//ultimo de la lista
     int indice_anterior=eliminado->anterior;
+    printf("tablahash[%i]=%i ahora es ",indiceEnTablaHash(eliminado->mascota.nombre),tablahash[indiceEnTablaHash(eliminado->mascota.nombre)]);
+    printf("tablahash[%i]=%i\n",indiceEnTablaHash(eliminado->mascota.nombre),indice_anterior);
     tablahash[indiceEnTablaHash(eliminado->mascota.nombre)]=indice_anterior;
     struct Nodo *anterior=LeerdeBD(indice_anterior);
     anterior->siguiente=-1;
     ActualizarMascotaenBD(indice_anterior,anterior);
     free(anterior);
   }else{//unico de la lista
+    printf("tablahash[%i]=%i ahora es ",indiceEnTablaHash(eliminado->mascota.nombre),tablahash[indiceEnTablaHash(eliminado->mascota.nombre)]);
+    printf("tablahash[%i]=%i\n",indiceEnTablaHash(eliminado->mascota.nombre),-2);
+    
     tablahash[indiceEnTablaHash(eliminado->mascota.nombre)]=-2;//se pone -2 en caso de que ya haya sido usado(problema: manejar el -2 al insertar)
   }
   
@@ -446,6 +461,10 @@ void EliminarDeLaBD(int posicion){
        actual->siguiente=(actual->siguiente-1);
      }
      if(i>posicion&&actual->siguiente==-1){
+    printf("reindexando:\n");
+    printf("tablahash[%i]=%i ahora es ",indiceEnTablaHash(actual->mascota.nombre),tablahash[indiceEnTablaHash(actual->mascota.nombre)]);
+    printf("tablahash[%i]=%i\n",indiceEnTablaHash(actual->mascota.nombre),i-1);
+    
        tablahash[indiceEnTablaHash(actual->mascota.nombre)]=i-1; //es el ultimo, actualiza la cabeza de lista
      }
      fwrite(actual,sizeof(struct Nodo),1,temporal);
